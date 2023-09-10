4fa4af2122ed7697ee244ed97f39ceb2bc74e4fb
Name <shi-666@yandex.ru>
2023-09-10 22:25:03 +0300

    binance done and working

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import websocket
+import json
+import pandas
+import numpy
+from binance.client import Client
 
 
-# НЕ РАБОТАЕТ. Но я хочу сохранить это в коммите
+# Binance API client
+api_key = 'your_api_key'
+api_secret = 'your_api_secret'
+client = Client(api_key, api_secret)
+
+symbol = 'BTCUSDT'
+interval = Client.KLINE_INTERVAL_5MINUTE
 
 price_changes = []
 gains = []
@@ -9,7 +19,6 @@ losses = []
 average_gain = 0
 average_loss = 0
 RSI = 0
-
 
 def calculate_RSI(price_change):
     global gains, losses, average_gain, average_loss, RSI
@@ -22,47 +31,52 @@ def calculate_RSI(price_change):
     gains.append(gain)
     losses.append(loss)
 
-    # Если достигнута длина 14 для расчета среднего прироста и средней потери
+    # Рассчет средних прироста и потери
     if len(gains) >= 14:
-        # Если это первое значение, инициализируем суммы прироста и потери
         if len(gains) == 14:
             average_gain = sum(gains) / 14
             average_loss = sum(losses) / 14
         else:
-            # Расчет среднего прироста и средней потери с использованием экспоненциального скользящего среднего
             average_gain = ((average_gain * 13) + gain) / 14
             average_loss = ((average_loss * 13) + loss) / 14
 
-        # Расчет relative_strength
+        # Рассчет relative strength и RSI
         relative_strength = average_gain / average_loss
-
-        # Расчет RSI по формуел
         RSI = 100 - (100 / (1 + relative_strength))
 
-    print(f"RSI: {RSI}")
+        print(f"RSI: {RSI}")
 
+def on_message(ws, message):
+    global price_changes
 
-def on_message(_wsa, data):
-    # Извлечение цены закрытия из полученных данных
-    close_price = float(data['k']['c'])
+    # парсим в JSON
+    data = json.loads(message)
 
-    # Вычисление изменения цены 
+    # Получаем данные из свечек
+    candlestick = data['k']
+    close_price = float(candlestick['c'])
+    open_price = float(candlestick['o'])
+    high_price = float(candlestick['h'])
+    low_price = float(candlestick['l'])
+
+    # Считаем изменение цены
     if len(price_changes) > 0:
         price_change = close_price - price_changes[-1]
         calculate_RSI(price_change)
 
-    # Добавление текущей цены закрытия в список
+    # Сюда добавляем цену закрытия в список price_changes 
     price_changes.append(close_price)
 
+    # Выводим данные из свечей
+    print(f"Open: {open_price}, High: {high_price}, Low: {low_price}, Close: {close_price}")
 
 def run():
-    print('hi')
+    # подключаемся через вебсокет 
+    stream_name = f"{symbol.lower()}@kline_{interval}"
+    wss = f"wss://stream.binance.com:9443/ws/{stream_name}"
+    ws = websocket.WebSocketApp(wss, on_message=on_message)
 
-    stream_name = 'btcusdt@kline_5m' # Пятиминутные свечи из доков Stream Name: <symbol>@kline_<interval>
-    wss = 'wss://stream.binance.com:9443/ws/%s' % stream_name
-    wsa = websocket.WebSocketApp(wss, on_message=on_message)
-    wsa.run_forever()
-
+    ws.run_forever()
 
 if __name__ == '__main__':
     run()
